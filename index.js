d1d8762d9f9d81804205749f4acd40588ae1349f
AbderrahimRezki <abderrahim4cem@gmail.com>
2022-07-24 17:48:17 +0100

    added update table button

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("ETC");
+const updateBtn = document.getElementById('update')
+const eventsTable = document.querySelectorAll('.inner-table')[1]
+const lastEventsRow = eventsTable.children[1].lastElementChild
+const instructor = lastEventsRow.children[2]
+const status = lastEventsRow.lastElementChild
+
+function updateTable(){
+    instructor.textContent = "Sohaib Zouambia"
+    status.textContent = "Done"
+    updateBtn.setAttribute('disabled', true)
+}
+
+updateBtn.addEventListener('click', updateTable)
